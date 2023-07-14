b35cfba5865e7d7f29bfc5737b31f74cd8d23a4b
RedNight_Lj <127466421+Ljduley12@users.noreply.github.com>
2023-07-13 22:15:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,27 @@ function loadContent(section) {
   } else if (section === "proxies") {
     content.innerHTML = "<h2>Proxies Section</h2><p>This is the Proxies section.</p>";
   } else if (section === "settings") {
-    content.innerHTML = "<h2>Settings Section</h2><p>This is the Settings section.</p>";
+    content.innerHTML = `
+      <h2>Settings Section</h2>
+      <div class="theme-option">
+        <h3>Theme</h3>
+        <label for="background-color">Background Color:</label>
+        <input type="color" id="background-color" onchange="changeBackgroundColor()" />
+      </div>
+      <div class="blank-option">
+        <h3>Blank</h3>
+        <button onclick="openInBlank()">Open in Blank</button>
+      </div>
+    `;
   }
+}
+
+function changeBackgroundColor() {
+  var colorInput = document.getElementById("background-color");
+  var body = document.querySelector("body");
+  body.style.backgroundColor = colorInput.value;
+}
+
+function openInBlank() {
+  window.open(location.href, "_blank");
 }
